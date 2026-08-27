b86f17b138401b7360f267d4d750fcb56b077879
tranhienchuong <118324170+tranhienchuong@users.noreply.github.com>
2026-08-27 21:21:48 +0700

    fix(auth): eliminate double IME padding by consuming Scaffold insets

--- feature/auth/src/main/java/com/tranhienchuong/nomad/feature/auth/AuthScreen.kt
+++ feature/auth/src/main/java/com/tranhienchuong/nomad/feature/auth/AuthScreen.kt
@@ -10,10 +10,10 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.consumeWindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawing
 import androidx.compose.foundation.layout.size
@@ -110,7 +110,7 @@ fun AuthScreen(
             modifier = Modifier
                 .fillMaxSize()
                 .padding(innerPadding)
-                .imePadding()
+                .consumeWindowInsets(innerPadding)
                 .verticalScroll(scrollState)
                 .padding(horizontal = 24.dp, vertical = 20.dp),
             horizontalAlignment = Alignment.CenterHorizontally,
